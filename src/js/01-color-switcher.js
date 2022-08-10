05721fa726afd16a0e9698f182700ea661cd9ac9
Serhii Zhyhanov <zhiganov76@gmail.com>
2022-08-10 21:27:07 +0300

    completed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyEl = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let timerId = null;
+
+btnStop.setAttribute('disabled', true);
+
+function onClickStartButton() {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+}
+
+function onClickStopButton() {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+}
+
+btnStart.addEventListener('click', onClickStartButton);
+btnStop.addEventListener('click', onClickStopButton);
